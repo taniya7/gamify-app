39f9c7cb81ae0dfed918e07c40c53c3a785afdac
Taniya Sarnobat <taniyasarnobat07@gmail.com>
2025-03-30 16:47:30 +0530

    Building Responsive Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Click Me</Button>
+      <Grid
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      >
+        <GridItem area="nav" bg="pink">
+          Nav
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem area="aside" bg="gray">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="yellow">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
 
 export default App;
+
+/* To add responsiveness :
+const breakpoints = {
+  base: 0px
+  sm: ~480px
+  md: ~768px
+  lg: ~992px
+  xl: ~1280px
+  "2xl": ~1536px
+}
+*/
