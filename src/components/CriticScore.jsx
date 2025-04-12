177896653be66aa8b8e9b4691e5fe6fcbe1ac496
Taniya Sarnobat <taniyasarnobat07@gmail.com>
2025-04-12 23:51:26 +0530

    Optimizing Images

--- src/components/CriticScore.jsx
+++ src/components/CriticScore.jsx
@@ -2,7 +2,6 @@ import { Badge } from "@chakra-ui/react";
 
 function CriticScore({ score }) {
   const color = score > 75 ? "green" : score > 60 ? "yellow" : "red";
-  // if score > 75 then "green", if score > 65 then "yellow", if score <= 65 then "red"
 
   return (
     <>
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,12 +1,13 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
+import CropImage from "../services/CropImage";
 
 function GameCard({ game }) {
   return (
     <>
       <Card borderRadius={10} overflow={"hidden"}>
-        <Image src={game.background_image} />
+        <Image src={CropImage(game.background_image)} />
         <CardBody>
           <Heading fontSize={"xl"}>{game.name}</Heading>
           <HStack justifyContent="space-between">
